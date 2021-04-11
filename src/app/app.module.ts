3743ed5fda39be6f5677729a4124bb96288cc71a
URIAN VIERA <urian1213viera@gmail.com>
2021-04-10 20:27:37 -0500

    iniciando mi proyecto de una calculadora completa con angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OperationsComponent } from './component/operations/operations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OperationsComponent
   ],
   imports: [
     BrowserModule
